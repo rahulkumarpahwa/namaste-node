013ccd1d9db321362b45c85498e0c10c2e4b5ecd
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-09-05 18:52:19 +0530

    S2-E12 completed

---
+++ S2-Episode-12/models/connectionRequest3.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const { User } = require("./userSchema");
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    // we will define here :
+    // 1. sender user, which sends the request to the other user.
+    //2. reciever user, which recieves the request from the reciever.
+    //3. Status of relation : ignored, interested, accepted, rejected.
+
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"], // Allowed values
+        message: "{VALUE} is not a valid status", // Custom error message
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// creating index (compound index) to make the query faster: 
+connectionRequestSchema.index({fromUserId: 1, toUserId : 1}); // 1 -> ascending
+// more at : https://www.mongodb.com/docs/manual/core/indexes/index-types/index-compound/
+
+
+// this is the middleware in the schema. It is called the 'pre' defined in the mongoose schema. here 'save' is the event before which it will take in action. don't use the arrow function.
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this; // any instance of the ConectionRequestModel
+  // check if the fromUserId is same as the toUserId.
+  if (connectionRequest.toUserId.equals(connectionRequest.fromUserId)) {
+    throw new Error("Sending Request to itself is not allowed!");
+  }
+  next(); // calling the next method of the middleware. must write this.
+});
+
+
+const ConnectionRequestModel = new mongoose.model(
+  "ConnectionRequestModel",
+  connectionRequestSchema
+);
+
+module.exports = ConnectionRequestModel;
